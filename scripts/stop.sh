e2052899461810f96a475c079cf2d4df56986e0a
swj8560 <120303851+swj8560@users.noreply.github.com>
2023-05-26 18:18:06 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,8 @@
 PROJECT_ROOT="/home/ubuntu/seb43_main_033"
 JAR_NAME="$PROJECT_ROOT/main/build/libs/main-0.0.1-SNAPSHOT.jar"
 
+touch "$PROJECT_ROOT/log"
+
 DEPLOY_LOG="$PROJECT_ROOT/log/deploy.log"
 
 TIME_NOW=$(date +%c)
