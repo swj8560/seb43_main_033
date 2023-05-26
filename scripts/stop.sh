f0cb068f7f5653e1eac93a8358b0210a4f04a609
swj8560 <120303851+swj8560@users.noreply.github.com>
2023-05-26 19:30:45 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -9,7 +9,7 @@ TIME_NOW=$(date +%c)
 
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
-echo "SERVER STOP.sh 실행"
+echo "SERVER STOP.sh 실행" >> $DEPLOY_LOG 
 
 if [ -z $CURRENT_PID ]; then
     echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
@@ -23,15 +23,14 @@ else
     if pgrep -f $JAR_NAME > /dev/null; then
         echo "$TIME_NOW > $CURRENT_PID 애플리케이션이 아직 종료되지 않았습니다. 5초 후에도 종료되지 않으면 강제 종료합니다." >> $DEPLOY_LOG
         sleep 5
-    else
-        echo "$TIME_NOW > $CURRENT_PID 애플리케이션이 정상적으로 종료되었습니다." >> $DEPLOY_LOG
-    fi
-   
-    if pgrep -f $JAR_NAME > /dev/null; then
-        echo "$TIME_NOW > $CURRENT_PID 애플리케이션 종료하지 못했습니다. 강제 종료합니다." >> $DEPLOY_LOG
-        kill -9 $CURRENT_PID
+        if pgrep -f $JAR_NAME > /dev/null; then
+            echo "$TIME_NOW > $CURRENT_PID 애플리케이션 종료하지 못했습니다. 강제 종료합니다." >> $DEPLOY_LOG
+            kill -9 $CURRENT_PID
+            echo "$TIME_NOW > $CURRENT_PID 애플리케이션 강제 종료 완료." >> $DEPLOY_LOG
+        else
+            echo "$TIME_NOW > $CURRENT_PID 애플리케이션이 정상적으로 종료되었습니다." >> $DEPLOY_LOG
+        fi
     else
         echo "$TIME_NOW > $CURRENT_PID 애플리케이션이 정상적으로 종료되었습니다." >> $DEPLOY_LOG
     fi
 fi
-
