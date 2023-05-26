2890395072ef908873c6a583bed7264079037c02
swj8560 <120303851+swj8560@users.noreply.github.com>
2023-05-26 19:33:14 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,11 +7,15 @@ DEPLOY_LOG="$PROJECT_ROOT/log/deploy.log"
 
 TIME_NOW=$(date +%c)
 
+echo "SERVER START.sh 실행" >> $DEPLOY_LOG 
+
 echo "$TIME_NOW > $JAR_NAME 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/main/build/libs/*.jar $JAR_NAME
 
 echo "$TIME_NOW > $JAR_NAME 파일 실행" >> $DEPLOY_LOG
 nohup java -jar $JAR_NAME > $APP_LOG 2> $ERROR_LOG &
+
+sleep 3
 
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
